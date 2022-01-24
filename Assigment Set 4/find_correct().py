07d1c3b84d80d22266b1b9e114b1435a7a54cbb3
Kishan Mishra <90112328+KishanMishra1@users.noreply.github.com>
2022-01-24 21:00:19 +0530

    Create find_correct().py

---
+++ Assigment Set 4/find_correct().py
@@ -0,0 +1,26 @@
+def find_correct(word_dict):
+    c1,c2,c3,cou=0,0,0,0
+    for i in word_dict:
+        if(word_dict[i]==i):
+            c1=c1+1
+        elif(len(word_dict[i])!=len(i)):
+            c3=c3+1
+        else:
+            x=word_dict[i]
+            for j in range(len(x)):
+                if(x[j]!=i[j]):
+                    cou=cou+1
+            if(cou<=2):
+                c2+=1
+            else:
+                c3+=1
+            cou=0
+    list=[c1,c2,c3]
+    return list
+        
+                
+                
+            
+
+word_dict={"THEIR": "THEIR","BUSINESS":"BISINESS","WINDOWS":"WINDMILL","WERE":"WEAR","SAMPLE":"SAMPLE"}
+print(find_correct(word_dict))
