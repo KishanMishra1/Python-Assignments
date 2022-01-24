3a4204a99e056c23750981a2545052a32e50b9aa
Kishan Mishra <90112328+KishanMishra1@users.noreply.github.com>
2022-01-24 17:04:39 +0530

    Create CheckPalindrome.py

---
+++ Assignment-3 -List/CheckPalindrome.py
@@ -0,0 +1,7 @@
+def check_palindrome(word):
+    return word==word[::-1]
+status=check_palindrome("malayalam")
+if(status):
+    print("word is palindrome")
+else:
+    print("word is not palindrome")
