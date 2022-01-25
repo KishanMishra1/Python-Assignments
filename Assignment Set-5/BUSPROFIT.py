92b60e8907bb9eeb21dcf10c9e4bd998c93e4bf9
Kishan Mishra <90112328+KishanMishra1@users.noreply.github.com>
2022-01-25 12:32:47 +0530

    Create BUSPROFIT.py

---
+++ Assignment Set-5/BUSPROFIT.py
@@ -0,0 +1,29 @@
+'''The road transport corporation (RTC) of a city wants to know whether a particular bus-route is running on profit or loss.
+
+Assume that the following information are given:
+Price per litre of fuel = 70
+Mileage of the bus in km/litre of fuel = 10
+Price(Rs) per ticket = 80
+
+The bus runs on multiple routes having different distance in kms and number of passengers.
+Write a function to calculate and return the profit earned (Rs) in each route. Return -1 in case of loss'''
+
+
+
+
+
+def calculate(distance,no_of_passengers):
+    
+    cost=70*distance/10
+    charge=80*no_of_passengers
+    if cost<=charge:
+        return abs(charge-cost)
+    else:
+        return -1
+
+
+
+#Provide different values for distance, no_of_passenger and test your program
+distance=20
+no_of_passengers=50
+print(calculate(distance,no_of_passengers))
